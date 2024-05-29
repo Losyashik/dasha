5c0ba9e6e97b503e3cdeb73b5aef4aef1dd4dd3d
Losyash <An.Shkenin@yandex.ru>
2024-05-29 18:09:14 +0300

    compleat

--- gallery.php
+++ gallery.php
@@ -2,8 +2,28 @@
 require_once("./assets/componetnts/header.php");
 ?>
 
+<main class="gallery">
+<h1 class="gallery_heading">
+    <hr class="gallery_heading-hr">
+    <span class="gallery_heading-text">Галлерея</span>
+    <hr class="gallery_heading-hr">
+</h1>
+<secttion class="gallery_body">
+    <img src="./assets/images/advantages/first.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/fourth.webp" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/second.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/third2.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/third.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/third1.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/second.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/first.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/third.jpg" alt="" class="gallery_image">
+    <img src="./assets/images/advantages/first.jpg" alt="" class="gallery_image">
+</secttion>
 
+</main>
 
+<?php require_once("./assets/componetnts/footer.php") ?>
 </body>
 
 </html>
